1dea51cf74aa3408529e49242a7b46a972208ee7
Anthony Rutherford <157150864+adrutherford@users.noreply.github.com>
2024-11-04 06:28:23 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Fall2024_Assignment3_adrutherford.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add configuration to access user secrets
+builder.Configuration.AddUserSecrets<Program>();
+
+// Retrieve the connection string from user secrets and add it to the DI container
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Use the connection string, for example in a DbContext (if using Entity Framework)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
